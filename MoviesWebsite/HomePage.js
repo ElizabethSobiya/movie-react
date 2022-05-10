6f850dda079cf91f957b816b2d5dd0e77852140c
Elizabeth Sobiya <98687741+ElizabethSobiya@users.noreply.github.com>
2022-05-10 19:27:00 +0530

    changes

--- MoviesWebsite/HomePage.js
+++ MoviesWebsite/HomePage.js
@@ -1,34 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import styles from './Movies.css'
+import React from 'react'
 
-function HomePage() {
-
-    const [movies, setMovies] = useState ([])
-
-    useEffect(() => {
-       axios.get('https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716')
-       .then( (res) => { setMovies(res.data.results)})
-    },[])
-
+function HomePage({poster_path,title}) {
   return (
-    <>
-    <h1> popular movies </h1>
-    <div id=  'movieWrapper'>
-      {movies.map ((movie, i) => {
-          return (
-            <div className= 'movieData'>
-                <img  className='moviePic' src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`} alt="" />
-               
-                <h2 className='movieTitle'>{movie.title}</h2>
-            </div>
-          )
-     
-          
-      })
-      }
-      </div>
-    </>
+      <div className= 'movieData'>
+                   <img  className='moviePic' src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`} alt="" />
+                 
+                   <h2 className='movieTitle'>{title}</h2>
+               </div>
   )
 }
 
